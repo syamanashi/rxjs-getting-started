35335e73e402211af2347aa271d15c23dfa12a64
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-28 10:51:11 -0400

    RxJS: Importing just what we need.

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
-import { Observable} from 'rxjs';
+// import { Observable } from 'rxjs';
+
+import { Observable } from 'rxjs/Observable';
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/filter";
 
 let numbers = [1, 5, 10];
 
