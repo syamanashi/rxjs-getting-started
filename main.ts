2da89654f837d00951fb6fc75297128bb656d591
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-28 13:06:05 -0400

    Working with Observable: processing mouse events - console.log() all mouse events - code cleanup

--- main.ts
+++ main.ts
@@ -1,44 +1,10 @@
-// import { Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
-import { Observable } from 'rxjs/Observable';
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/filter";
+// Observable.fromEvent() instructs RxJS to wire up an event handler on a DOM element (e.g. button click event):
+let source = Observable.fromEvent(document, "mousemove");
 
-let numbers = [1, 5, 10];
-
-// Create an observable using the Observable.create() method, passing in a function that takes as a parameter an Observer object. 
-//      This approach allows the Observable to interact with an Observer directly:
-
-// Make this oberservable asynchonous using setTimeout.
-//      Most observables are asynchonous, producing a stream of data over time:
-//      They deliver data in the future, or when some specific event occurs, like a mouse click event.
-let source = Observable.create(observer => {
-    
-    let index = 0;
-
-    // create a function that invokes observer.next(value) and passes in numbers[index].
-    let produceValue = () => {
-
-        // next passes a value to the observer, then immediately increments index:
-        observer.next(numbers[index++]);
-
-        if (index < numbers.length) {
-            setTimeout(produceValue, 250);
-        } else {
-            observer.complete();
-        }
-    }
-    produceValue();
-
-}).map(n => n * 2)
-  .filter(n => n > 4);
-
-// NOTE: The observer is simply an object that waits for data to be produced from the observable, and provides the logic to react to the data that is arriving.
-// Simpler way to build an observer:
-//      Invoke the Observable.subscribe() method without a class in a more simple approach:
-//      Note: the complete handler does not receive any parameters, we need to use the ()
 source.subscribe(
-    value => console.log(`value: ${value}`),
+    value => console.log(value),
     e => console.log(`error: ${e}`),
     () => console.log("complete")
 );
