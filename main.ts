258a14551fc0c3077b9c0fd67d023dd089989d90
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-28 14:10:59 -0400

    Working with Observables: Sending Requests with XmlHttpRequest (XHR): - (w/out RxJS) - code cleanup

--- main.ts
+++ main.ts
@@ -1,27 +1,37 @@
 import { Observable } from 'rxjs';
 
-// get reference to the DOM element of the #circle id:
-let circle = document.getElementById("circle");
+// get reference to the DOM element of the #output and #button ids:
+let output = document.getElementById("output");
+let button = document.getElementById("button");
 
-// Observable.fromEvent() instructs RxJS to wire up an event handler on a DOM element (e.g. button click event):
-let source = Observable.fromEvent(document, "mousemove")
-                       .map((e : MouseEvent) => {
-                           return {
-                               x: e.clientX,
-                               y: e.clientY
-                           }
-                       })
-                       .filter(value => value.x < 500)
-                       .delay(300);
+// Observable.fromEvent() instructs RxJS to wire up an event handler on the button DOM element (e.g. button click event):
+let click = Observable.fromEvent(button, "click");
 
-function onNext(value) {
-    // console.log(value.x + ' x ' + value.y);
-    circle.style.left = value.x + 'px';
-    circle.style.top = value.y + 'px';
+// Review XMLHttpRequest: 
+//      https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+//      https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/open
+//      https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/send
+function getMovies(url: string) {
+    let xhr = new XMLHttpRequest();
+
+    // handle the xhr 'load' event which is raised when the data arrives back from the web server.
+    xhr.addEventListener("load", () => {
+        let movies = JSON.parse(xhr.responseText);
+        
+        // loop through the resulting array:
+        movies.forEach(m => {
+            let div = document.createElement("div");
+            div.innerText = m.title;
+            output.appendChild(div);
+        });
+    });
+
+    xhr.open("GET", url); // sets up the request type, location
+    xhr.send(); // send the request asynchronously
 }
 
-source.subscribe(
-    onNext,
+click.subscribe(
+    e => getMovies("movies.json"),
     e => console.log(`error: ${e}`),
     () => console.log("complete")
 );
