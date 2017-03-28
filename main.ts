1d92a08ab7a0ab44e84e3995e0a9fff59b139507
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-28 10:12:37 -0400

    going async with setTimeout - the Observer does not change as it is isolated from the details of the Observable.

--- main.ts
+++ main.ts
@@ -4,21 +4,31 @@ let numbers = [1, 5, 10];
 
 // Create an observable using the Observable.create() method, passing in a function that takes as a parameter an Observer object. 
 //      This approach allows the Observable to interact with an Observer directly:
+
+// Make this oberservable asynchonous using setTimeout.
+//      Most observables are asynchonous, producing a stream of data over time:
+//      They deliver data in the future, or when some specific event occurs, like a mouse click event.
 let source = Observable.create(observer => {
     
-    for (let n of numbers) {
-        
-        // if (n === 5) {
-        //     observer.error("Something went wrong!");
-        // }
-        
-        observer.next(n);
-    }
+    let index = 0;
 
-    observer.complete();
+    // create a function that invokes observer.next(value) and passes in numbers[index].
+    let produceValue = () => {
+
+        // next passes a value to the observer, then immediately increments index:
+        observer.next(numbers[index++]);
+
+        if (index < numbers.length) {
+            setTimeout(produceValue, 2000);
+        } else {
+            observer.complete();
+        }
+    }
+    produceValue();
 
 });
 
+// NOTE: The observer is simply an object that waits for data to be produced from the observable, and provides the logic to react to the data that is arriving.
 // Simpler way to build an observer:
 //      Invoke the Observable.subscribe() method without a class in a more simple approach:
 //      Note: the complete handler does not receive any parameters, we need to use the ()
