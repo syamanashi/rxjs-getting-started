566e86e936a5fab65b136d06c0db958f0f7dc69e
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-28 08:36:29 -0400

    Finish Webpack configuration with webpack-dev-server --watch --inline running on port 8080 - webpack loaders are tools that can look at different filetypes, process those files, and give webpack the output to bundle into the single .js file as output. - ts-loader is a webpack plugin that knows how to process TypeScript files - "postinstall": "typings install" in package.json automatically runs immediately (the typings shim installed package) after a user pulls down project and runs $ npm install

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,15 @@
 module.exports = {
     entry: "./main",
-    output: { filename: "app.js"}
+    output: { filename: "app.js"},
+    module: {
+        loaders: [
+            {
+                test: /.ts$/,
+                loader: "ts-loader"
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    }
 }
