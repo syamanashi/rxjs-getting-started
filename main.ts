5c79c3c73dd3d1754e419cac767acfff2c5c480c
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-03-28 09:46:43 -0400

    using Observable.create

--- main.ts
+++ main.ts
@@ -1,9 +1,23 @@
-import { Observable, Observer} from 'rxjs';
+import { Observable} from 'rxjs';
 
 let numbers = [1, 5, 10];
 
-// create a simple Observable datasource from the array.
-let source = Observable.from(numbers);
+// Create an observable using the Observable.create() method, passing in a function that takes as a parameter an Observer object. 
+//      This approach allows the Observable to interact with an Observer directly:
+let source = Observable.create(observer => {
+    
+    for (let n of numbers) {
+        
+        // if (n === 5) {
+        //     observer.error("Something went wrong!");
+        // }
+        
+        observer.next(n);
+    }
+
+    observer.complete();
+
+});
 
 // Simpler way to build an observer:
 //      Invoke the Observable.subscribe() method without a class in a more simple approach:
